978d36a4f7c673c3ea3d9f6cb7d05bc92ae9c8a8
Alexandre Fernandez <contact@alexandrefernandez.me>
2022-02-06 14:41:41 +0100

    better config.example

--- react/config/config.example.ts
+++ react/config/config.example.ts
@@ -1,7 +1,29 @@
-const dev = {}
+const prodHost = "docker"
 
-const prod = {}
+type configuration = {
+	api: {
+		register: string,
+		login: string,
+		products: string
+	}
+}
 
-const config = process.env.NODE_ENV === "development" ? dev : prod
+const dev: configuration = {
+	api: {
+		register: "/register",
+		login: "/api/login_check",
+		products: "/api/product",
+	}
+}
+
+const prod: configuration = {
+	api: {
+		register: `${prodHost}/register`,
+		login: `${prodHost}/api/login_check`,
+		products: `${prodHost}/api/product`
+	}
+}
+
+const config: configuration = process.env.NODE_ENV === "development" ? dev : prod
 
 export default config
